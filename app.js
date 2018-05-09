21dae1df1e1534569b52e927b6553eb75c1723a4
ana-mihaela <ana.mihaela@ymail.com>
2018-05-09 15:32:43 +0300

    -

--- app.js
+++ app.js
@@ -40,18 +40,6 @@ app.use('/chronicles', chronicleRoutes);
 app.use('/preloaders', preloadRoutes);
 app.use('/users', userRoutes);
 
-/*CORS*/
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    // treats OPTIONS sent by the browser before posting
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
-        return res.status(200).json({});
-    };
-    next(); // invoke next
-});
-
 /*Create the error handling mechanism*/
 app.use((req, res, next) => {
     const error = new Error('Resource not found');
